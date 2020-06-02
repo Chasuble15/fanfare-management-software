1ca6f094fb3877887a33fc543924ab15d08f23cb
Thibaud <carruptt@gmail.com>
2020-06-02 14:54:07 +0200

    Add modify and delete

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,11 +1,67 @@
 <template>
   <div class="members">
+    <b-col cols="4">
+      <b-form-group label-for="filterInput" label-cols-sm="1">
+        <b-input-group size="sm">
+          <b-form-input v-model="filter" type="search" id="filterInput" placeholder="Rechercher..."></b-form-input>
+          <b-input-group-append>
+            <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')">Nouveau membre</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </b-col>
+
     <div>
-      <b-table hover bordered head-variant="light" :items="memberList" :fields="fields"></b-table>
+      <b-table
+        hover
+        bordered
+        head-variant="light"
+        :items="memberList"
+        :fields="fields"
+        :filter="filter"
+      >
+        <template v-slot:cell(actions)="row">
+          <b-button
+            size="sm"
+            @click="info(row.item, row.index, $event.target)"
+            class="mr-1"
+          >Modifier</b-button>
+          <b-button size="sm" @click="deleteMember(row.item)" class="mr-1">Supprimer</b-button>
+        </template>
+
+        <template v-slot:row-details="row">
+          <b-card>
+            <ul>
+              <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+            </ul>
+          </b-card>
+        </template>
+      </b-table>
+
+      <b-modal :id="infoModal.id" title="Modifier un membre" hide-footer>
+        <div class="form-group">
+          <label for="firstName">Prénom</label>
+          <input type="text" class="form-control" id="firstName" v-model="infoModal.firstName" />
+        </div>
+        <div class="form-group">
+          <label for="lastName">Nom</label>
+          <input type="text" class="form-control" id="lastName" v-model="infoModal.lastName" />
+        </div>
+        <div class="form-group">
+          <label for="newInstrument">Instrument</label>
+          <input type="text" class="form-control" id="newInstrument" v-model="infoModal.instrument" />
+        </div>
+        <b-row>
+          <b-col>
+            <b-button variant="success" block @click="modifyMember">Modifier</b-button>
+          </b-col>
+          <b-col>
+            <b-button variant="danger" block @click="cancelModify">Annuler</b-button>
+          </b-col>
+        </b-row>
+      </b-modal>
     </div>
     <div>
-      <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')">Nouveau membre</b-button>
-
       <b-modal id="bv-modal-example" hide-footer>
         <template v-slot:modal-title>Nouveau membre</template>
         <div class="form-group">
@@ -38,7 +94,7 @@
             v-model="instrument"
           />
         </div>
-        <b-button class="mt-3" block @click="addMember">Valider</b-button>
+        <b-button variant="success" block @click="addMember">Valider</b-button>
       </b-modal>
     </div>
   </div>
@@ -67,12 +123,22 @@ export default {
           key: "instrument",
           label: "Instrument",
           sortable: true
-        }
+        },
+        { key: "actions", label: "Actions" }
       ],
       memberList: [],
       lastName: "",
       firstName: "",
-      instrument: ""
+      instrument: "",
+      filter: null,
+      infoModal: {
+        id: "info-modal",
+        firstName: "",
+        lastName: "",
+        instrument: "",
+        key: ""
+      },
+      dMember: {}
     };
   },
   firestore() {
@@ -83,21 +149,63 @@ export default {
   methods: {
     addMember: function() {
       var id = this.memberList.length + 1;
-      this.$firestore.memberList.add({
-        key: id,
-        lastName: this.lastName,
-        firstName: this.firstName,
-        instrument: this.instrument
-      });
+      this.$firestore.memberList
+        .add({
+          id: id,
+          lastName: this.lastName,
+          firstName: this.firstName,
+          instrument: this.instrument
+        })
+        .then(() => {
+          // Hide the modal manually
+          this.$nextTick(() => {
+            this.$bvModal.hide("bv-modal-example");
+          });
 
-      // Hide the modal manually
+          this.lastName = "";
+          this.firstName = "";
+          this.instrument = "";
+        });
+    },
+    modifyMember() {
+      db.collection("memberList")
+        .doc(this.infoModal.key)
+        .update({
+          firstName: this.infoModal.firstName,
+          lastName: this.infoModal.lastName,
+          instrument: this.infoModal.instrument
+        })
+        .then(() => {
+          this.$nextTick(() => {
+            this.$bvModal.hide(this.infoModal.id);
+          });
+          //Reset modal
+          this.infoModal.firstName = "";
+          this.infoModal.lastName = "";
+          this.infoModal.instrument = "";
+          this.infoModal.key = "";
+        });
+    },
+    cancelModify() {
       this.$nextTick(() => {
-        this.$bvModal.hide("bv-modal-example");
+        this.$bvModal.hide(this.infoModal.id);
       });
+      //Reset modal
+      this.infoModal.firstName = "";
+      this.infoModal.lastName = "";
+      this.infoModal.instrument = "";
+      this.infoModal.key = "";
+    },
+    info(item, index, button) {
+      this.infoModal.firstName = item.firstName;
+      this.infoModal.lastName = item.lastName;
+      this.infoModal.instrument = item.instrument;
+      this.infoModal.key = item[".key"];
 
-      this.lastName = "";
-      this.firstName = "";
-      this.instrument = "";
+      this.$root.$emit("bv::show::modal", this.infoModal.id, button);
+    },
+    deleteMember(member) {
+      this.$firestore.memberList.doc(member[".key"]).delete();
     }
   }
 };
